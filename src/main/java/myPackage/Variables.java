ada8affefa08848dec745e08025bb1d9df638cfa
Michael Yaroshevsky <yaroshevsky1995@gmail.com>
2019-09-17 15:01:39 +0200

    Modified enum class

--- src/main/java/myPackage/Variables.java
+++ src/main/java/myPackage/Variables.java
@@ -1,27 +1,38 @@
 package myPackage;
 
 public enum Variables {
-    WBANNO,
-    UTC_DATE,
-    UTC_TIME,
-    LST_DATE,
-    LST_TIME,
-    CRX_VN,
-    LONGITUDE,
-    LATITUDE,
-    AIR_TEMPERATURE,
-    PRECIPITATION,
-    SOLAR_RADIATION,
-    SR_FLAG,
-    SURFACE_TEMPERATURE,
-    ST_TYPE,
-    ST_FLAG,
-    RELATIVE_HUMIDITY,
-    RH_FLAG,
-    SOIL_MOISTURE_5,
-    SOIL_TEMPERATURE_5,
-    WETNESS,
-    WET_FLAG,
-    WIND_1_5,
-    WIND_FLAG
+    WBANNO(false),
+    UTC_DATE(false),
+    UTC_TIME(false),
+    LST_DATE(false),
+    LST_TIME(false),
+    CRX_VN(false),
+    LONGITUDE(false),
+    LATITUDE(false),
+    AIR_TEMPERATURE(true),
+    PRECIPITATION(true),
+    SOLAR_RADIATION(true),
+    SR_FLAG(false),
+    SURFACE_TEMPERATURE(true),
+    ST_TYPE(false),
+    ST_FLAG(false),
+    RELATIVE_HUMIDITY(true),
+    RH_FLAG(false),
+    SOIL_MOISTURE_5(true),
+    SOIL_TEMPERATURE_5(true),
+    WETNESS(false),
+    WET_FLAG(false),
+    WIND_1_5(true),
+    WIND_FLAG(false);
+
+
+    private boolean testRequired;
+
+    Variables(boolean testRequired){
+        this.testRequired = testRequired;
+    }
+
+    public boolean isTestRequired() {
+        return testRequired;
+    }
 }
